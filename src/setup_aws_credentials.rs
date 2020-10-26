f71b1749d8bc91e6f4bbf8a97a95225eb2feae26
EC2 Default User <ec2-user@ip-10-1-6-140.ap-southeast-2.compute.internal>
2020-10-26 02:41:52 +0000

    fixing permissions

--- src/setup_aws_credentials.rs
+++ src/setup_aws_credentials.rs
@@ -1,24 +1,29 @@
 use rusoto_credential::{
-    AwsCredentials, ChainProvider, ProfileProvider,
-    ProvideAwsCredentials,
+    AwsCredentials, ChainProvider, ProfileProvider,DefaultCredentialsProvider,ProvideAwsCredentials,
 };
 use std::time::Duration;
 
 pub async fn fetch_credentials() -> (AwsCredentials, String) {
-    let profile_name = "pym-disk";
+    
+    //let profile_name = "pym-disk";
     //let profile_name = "default";
 
 
-    let mut pp = ProfileProvider::new().unwrap();
-    pp.set_profile(profile_name);
-    let mut cp = ChainProvider::with_profile_provider(pp);
+    //let mut pp = ProfileProvider::new().unwrap();
+    //pp.set_profile(profile_name);
+    //let mut cp = ChainProvider::with_profile_provider(pp);
+    let mut cp = ChainProvider::new();
     // out expectation is to be running in AWS so this is plenty of time for it to
     // get any EC2 role credentials
     cp.set_timeout(Duration::from_millis(500));
-    let creds = cp.credentials().await.unwrap();
-
+    //let creds = cp.credentials().await.unwrap();
+    let creds = DefaultCredentialsProvider::new()
+            .unwrap()
+            .credentials()
+            .await
+            .unwrap();
     (
         creds.clone(),
-        format!("Profile `{}` -> {:?}", profile_name, creds,),
+        format!("{:?}", creds)
     )
 }
